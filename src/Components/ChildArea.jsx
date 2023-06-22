3313b6901a804f20bd45e35660d4aa1bc50e4d1c
KEITA <ksekimotonz@gmail.com>
2023-06-22 09:49:49 +0900

    レンダリングを学ぶためにchildAreaブランチを追加

--- src/Components/ChildArea.jsx
+++ src/Components/ChildArea.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 const ChildArea = (props) => {
   const { open } = props;
+
+  console.log("レンダリングがまもなく開始します。");
+
+  const data = [...Array(2000).keys()];
+  console.log("レンダリング終わりました");
+
   return (
     <>
       {open ? (
