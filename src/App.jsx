45750622183900b84a507445d8014ee3f6368217
KEITA <ksekimotonz@gmail.com>
2023-06-27 17:50:32 +0900

    サーバーを立てなくても、axiosでデータを取得できるので便利

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import './App.css';
-import { UserProvider } from './Providers/UserProvider';
-import { Router } from "./Router/Router";
-
-
+import axios from "axios";
 
 function App() {
+
+  const onClickUsers = () => {
+    // alert("users");
+
+    // .then()でデータを取得した後に実行する関数を指定できる
+    axios.get("https://jsonplaceholder.typicode.com/users").then((result) => {
+      console.log(result.data);
+    })
+    .catch((err) => console.log(err))
+  }
+  const onClickUser1 = () => {
+    axios.get("https://jsonplaceholder.typicode.com/users/3").then((result) => {
+      console.log(result.data);
+    })
+    .catch((err) => console.log(err))
+}
+
+
   return (
-    <UserProvider>
-      <Router />
-    </UserProvider>
+    <>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </>
   );
 }
 
